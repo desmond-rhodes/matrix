48ca84afa7f65aeb86a41bd5b45b918c06c858a5
Desmond Rhodes <desmond.rhodes@outlook.com>
2022-03-12 18:33:26 +0700

    Matrix multiplication and equality

--- main.cc
+++ main.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
 #include "matrix.hh"
+
+template <typename T> std::ostream& operator<<(std::ostream&, matrix<T> const&);
 
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(false);
@@ -12,15 +15,60 @@ int main(int argc, char* argv[]) {
 		std::cout << i << ' ';
 	std::cout << '\n';
 
-	matrix<int> m {3, 3, 1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9};
-	print(m);
+	matrix<double> I {2, 2, 1.0, 0.0, 0.0, 1.0};
+	matrix<double> A {2, 2, 1.0, 2.0, 3.0, 4.0};
+	matrix<double> B {2, 2, 5.0, 6.0, 7.0, 8.0};
+	matrix<double> v {2, 1, 9.0, 10.0};
+
+	std::cout << "I\n" << I << "\n\n";
+	std::cout << "A\n" << A << "\n\n";
+	std::cout << "B\n" << B << "\n\n";
+	std::cout << "v\n" << v << "\n\n";
+	std::cout << "I == I " << (I == I) << "\n\n";
+	std::cout << "A != A " << (A != A) << "\n\n";
+	std::cout << "I == A " << (I == A) << "\n\n";
+	std::cout << "A != I " << (A != I) << "\n\n";
+	std::cout << "A*I\n" << A*I << "\n\n";
+	std::cout << "I*A\n" << I*A << "\n\n";
+	std::cout << "Av\n" << (A*v) << "\n\n";
+	std::cout << "Bv\n" << (B*v) << "\n\n";
+	std::cout << "AI == IA " << (A*I == I*A) << "\n\n";
+	std::cout << "AB == BA " << (A*B == B*A) << "\n\n";
+	std::cout << "I == v " << (I == v) << "\n\n";
+
+	matrix<unsigned long long> F0 {2, 2, 1ul, 1ul, 1ul, 0ul};
+	matrix<unsigned long long> Fn {2, 2, 1ul, 0ul, 0ul, 1ul};
+	std::cout << "Fibonacci matrix\n\n";
+	for (size_t i {0}; i < 10; ++i)
+		std::cout << (Fn *= F0) << "\n\n";
 
 	std::cout << "Hello, world!\n";
 	return 0;
 }
 
 template <typename T>
-void print(matrix<T> m) {
-	for (size_t i {0}; i < m.data.size(); ++i)
-		std::cout << m.data[i] << ((i%m.col == m.col-1) ? '\n' : ' ');
+std::ostream& operator<<(std::ostream& os, matrix<T> const& m) {
+	std::vector<size_t> align(m.col());
+	for (auto& i : align)
+		i = 0;
+
+	for (size_t i {0}; i < m.row(); ++i) {
+		for (size_t j {0}; j < m.col(); ++j) {
+			auto length {std::to_string(m(i, j)).size()};
+			if (align[j] < length)
+				align[j] = length;
+		}
+	}
+
+	for (size_t i {0}; i < m.row(); ++i) {
+		for (size_t j {0}; j < m.col(); ++j) {
+			os << std::setw(align[j]) << std::to_string(m(i, j));
+			if (j != m.col()-1)
+				os << ' ';
+		}
+		if (i != m.row()-1)
+			os << '\n';
+	}
+
+	return os;
 }
--- matrix.hh
+++ matrix.hh
@@ -6,19 +6,27 @@
 template <typename T>
 class matrix {
 	public:
-		template <typename ...U>
-		matrix(size_t, size_t, U...);
+		template <typename ...U> matrix(size_t, size_t, U...);
 
-		template <typename U>
-		friend void print(matrix<U>);
+		size_t row() const;
+		size_t col() const;
+
+		T& operator()(size_t, size_t);
+		T const& operator()(size_t, size_t) const;
 
 	private:
 		matrix(size_t, size_t, std::initializer_list<T>);
 
-		size_t row;
-		size_t col;
-		std::vector<T> data;
+		size_t _row;
+		size_t _col;
+		std::vector<T> _data;
 };
+
+template <typename T> bool operator==(matrix<T> const&, matrix<T> const&);
+template <typename T> bool operator!=(matrix<T> const&, matrix<T> const&);
+
+template <typename T> matrix<T> operator*(matrix<T> const&, matrix<T> const&);
+template <typename T> matrix<T>& operator*=(matrix<T>&, matrix<T> const&);
 
 #include "matrix.tt"
 
